ea560fce9a01f97a61ea53e2cc5eb8bba5e44929
vinoth <webdeveloper2@sonatech.ac.in>
2026-06-30 16:55:34 +0530

    pusggg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Script from "next/script";
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DV9H87WQSY"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-DV9H87WQSY');
+        `}
+      </Script>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
