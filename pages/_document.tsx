e48dd58a02a563e955cff5163b5bd665fcc8018e
vinoth <webdeveloper2@sonatech.ac.in>
2026-09-07 09:49:12 +0530

    pushh

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,8 +19,36 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1567882758320307');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </Head>
       <body>
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1567882758320307&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
